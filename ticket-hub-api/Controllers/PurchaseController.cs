80430322f8793a44f1e1d46d45d2b694d35b75fe
heosmond <osmondhe@gmail.com>
2025-03-26 15:17:15 -0300

    Add project files.

---
+++ ticket-hub-api/Controllers/PurchaseController.cs
@@ -0,0 +1,51 @@
+﻿using Azure.Storage.Queues;
+using Microsoft.AspNetCore.Mvc;
+using System.Runtime.InteropServices.Marshalling;
+using System.Text.Json;
+
+namespace ticket_hub_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PurchaseController : ControllerBase
+    {
+        private readonly ILogger<PurchaseController> _logger;
+        private readonly IConfiguration _configuration;
+
+        public PurchaseController(ILogger<PurchaseController> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            _configuration = configuration;
+        }
+
+        // TODO In portal.azure.com, go to the Web App, then go to Settings -> Environment Variables. Click the "+" to add a new "App Setting"
+        [HttpPost]
+        public async Task<IActionResult> Post(Purchase purchase)
+        {
+            string? connectionString = _configuration["AzureStorageConnectionString"];
+
+            if (string.IsNullOrEmpty(connectionString))
+            {
+                return BadRequest("An error was encountered");
+            }
+
+            if (ModelState.IsValid == false)
+            {
+                return BadRequest(ModelState);
+            }
+
+            // send ticket request to Azure
+            string queueName = "tickethub";
+            QueueClient queueClient = new QueueClient(connectionString, queueName);
+
+            // serialize an object to json
+            string message = JsonSerializer.Serialize(purchase);
+
+            // send string message to queue
+            await queueClient.SendMessageAsync(message);
+
+
+            return Ok("Thank you, " + purchase.Name + ". Your purchase is being processed");
+        }
+    }
+}
---
+++ ticket-hub-api/Purchase.cs
@@ -0,0 +1,72 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ticket_hub_api
+{
+    public class Purchase
+    {
+        [Required]
+        public int ConcertId { get; set; }
+
+
+        [EmailAddress]
+        [MaxLength(255)]
+        [Required(ErrorMessage = "Email is required")]
+        public string Email { get; set; } = string.Empty;
+
+
+        [RegularExpression(@"^[a-zA-Z'\s]{1,50}$", ErrorMessage = "Characters are not allowed.")]
+        [Required(ErrorMessage = "Name is required")]
+        public string Name { get; set; } = string.Empty;
+
+
+        [Phone]
+        [Required(ErrorMessage = "Phone is required")]
+        public string Phone { get; set; } = string.Empty;
+
+
+        [Range(1, 6, ErrorMessage = "Quantity must be between 1 and 6")]
+        [Required]
+        public int Quantity { get; set; }
+
+
+        [CreditCard]
+        [Required(ErrorMessage = "Credit card number is required")]
+        public string CreditCard { get; set; } = string.Empty;
+
+
+        [RegularExpression(@"^(0[1-9]|1[0-2])\/?([0-9]{4}|[0-9]{2})$", ErrorMessage = "Invalid expiration")]
+        [Required(ErrorMessage = "Credit card expiration is required")]
+        public string Expiration { get; set;} = string.Empty;
+
+
+        [RegularExpression(@"^[0-9]{3}$", ErrorMessage = "Invalid security code")]
+        [Required(ErrorMessage = "Credit card security code is required")]
+        public string SecurityCode { get; set; } = string.Empty;
+
+
+        [MaxLength(95)]
+        [Required(ErrorMessage = "Address is required")]
+        public string Address { get; set; } = string.Empty;
+
+
+        [MaxLength(45)]
+        [Required(ErrorMessage = "City is required")]
+        public string City { get; set; } = string.Empty;
+
+
+        [MaxLength(20)]
+        [Required(ErrorMessage = "Province is required")]
+        public string Province { get; set; } = string.Empty;
+
+
+        [MaxLength(12)]
+        [RegularExpression(@"^[A-Za-z]\d[A-Za-z][ -]?\d[A-Za-z]\d$", ErrorMessage = "Invalid postal code format")]
+        [Required(ErrorMessage = "Postal code is required")]
+        public string PostalCode { get; set; } = string.Empty;
+
+        [MaxLength(90)]
+        [Required(ErrorMessage = "Country is required")]
+        public string Country { get; set; } = string.Empty;
+
+    }
+}
